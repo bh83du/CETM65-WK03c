a79174c8eb3634cd00e382e689cac52c00ca695b
bh83du <bh83du@student.sunderland.ac.uk>
2020-10-29 19:11:57 +0000

    Updated templates and app.py

--- app.py
+++ app.py
@@ -1,15 +1,31 @@
+# Import Flask and render-template.
 from flask import Flask, render_template
+
+# Define the app 
+
 app = Flask(__name__)
+
+# Add function for the default route
 
 @app.route('/')
 def index():
     users = ['Stuart','Joe','Frank']
     return render_template('index.html', title = 'Home', users=users)
 
+# Add further functions for the User routes.
+
 @app.route('/Stuart')
-def my_func_2():
-    return 'Hi from the Test Route!'
+def stuart():
+    return render_template('user.html', title = 'Home', users='Stuart')
+
+@app.route('/Joe')
+def joe():
+    return render_template('user.html', title = 'Home', users='Joe')
+
+@app.route('/Frank')
+def frank():
+    return render_template('user.html', title = 'Home', users='Frank')
 
 
-
+# Debugging is ON
 app.run(debug=True)
