62354e729b393980ed2836b319be34a68133f3d9
bh83du <bh83du@student.sunderland.ac.uk>
2020-10-29 19:16:55 +0000

    Updated comments on app.py

--- app.py
+++ app.py
@@ -13,7 +13,10 @@ def index():
     return render_template('index.html', title = 'Home', users=users)
 
 # Add further functions for the User routes.
-
+# Each route takes the end user to a page for the selected User.
+# User page is an extension of the layout.html
+# Can be used to produce identical pages for different users
+  
 @app.route('/Stuart')
 def stuart():
     return render_template('user.html', title = 'Home', users='Stuart')
